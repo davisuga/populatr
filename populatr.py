5f4e76f5c88f908314076717b695b106cf037b6f
Davi William Moraes Suga <33470530+daviwms999@users.noreply.github.com>
2019-09-10 15:04:36 -0300

    Update populatr.py

--- populatr.py
+++ populatr.py
@@ -1,12 +1,22 @@
 users=[]
-nomes = open('nomes.txt', 'r')
-nomes = nomes.readlines()
-print(nomes)
-#nomes = map(lambda s: s.strip(), nomes)
-
-#print(nomes)
 populatr = open('populatr.sql', 'w+')
-for nome in nomes:
-    line="insert into usuario values(NULL,'{}','{}','{}','{}');\n".format(nome.strip(),nome.strip()[::-1],nome.strip()+'@gmail.com',nome.strip()+".png")
-    populatr.write(line)
+
+#nomes = map(lambda s: s.strip(), nomes)
+def populateUsers():
+    nomes = open('nomes.txt', 'r')
+    nomes = nomes.readlines()
+    print(nomes)    
+    #print(nomes)
+    for nome in nomes:
+        line="insert into usuario values(NULL,'{}','{}','{}','{}');\n".format(nome.strip(),nome.strip()[::-1],nome.strip()+'@gmail.com',nome.strip()+".png")
+        populatr.write(line)
+
+def populateIngredients():
+    ingredients = open('ingredients.txt', 'r')
+    ingredients = ingredients.readlines()
+    print(ingredients)
+    for ingredient in ingredients:
+        line="insert into usuario values(NULL,'{}');\n".format(ingredient.strip())
+        populatr.write(line)
+
 populatr.close()
